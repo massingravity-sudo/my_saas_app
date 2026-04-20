5e0038eb15ec0ebbc1467128c1f05a6288fc40d3
massingravity-sudo <massingravity@gmail.com>
2026-04-20 23:24:13 +0100

    fix jwt

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from datetime import datetime, timedelta
-import jwt
+from PyJWT import jwt
 import secrets
 import smtplib
 from email.mime.text import MIMEText
